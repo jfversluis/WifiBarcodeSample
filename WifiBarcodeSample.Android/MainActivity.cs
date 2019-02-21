baab84ba800be0b25e01446ea03a1bbc65c29434
Gerald Versluis <gerald@verslu.is>
2019-02-21 16:27:33 +0100

    Initial commit

---
+++ WifiBarcodeSample.Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace WifiBarcodeSample.Droid
+{
+	[Activity(Label = "WifiBarcodeSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(savedInstanceState);
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+			ZXing.Net.Mobile.Forms.Android.Platform.Init();
+			LoadApplication(new App());
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+		{
+			global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
+}
---
+++ WifiBarcodeSample/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Xamarin.Forms;
+using ZXing;
+
+namespace WifiBarcodeSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		public void Generate_Barcode(object sender, EventArgs e)
+		{
+			BarcodeScanView.IsVisible = false;
+			BarcodeScanView.IsScanning = false;
+
+			// TODO Implement error handling and validation
+			var security = "";
+			var ssidHidden = "";
+
+			switch (Security.SelectedIndex)
+			{
+				case 0:
+					security = "WPA";
+					break;
+				case 1:
+					security = "WEP";
+					break;
+				default:
+					security = "";
+					break;
+			}
+
+			if (HiddenSsid.IsToggled)
+				ssidHidden = "H:true";
+
+			BarcodeImageView.BarcodeValue = $"WIFI:S:{Ssid.Text};T:{security};P:{Password.Text};{ssidHidden};";
+
+			BarcodeImageView.IsVisible = true;
+		}
+
+		public void Scan_Barcode(object sender, EventArgs e)
+		{
+			BarcodeImageView.IsVisible = false;
+
+			BarcodeScanView.IsVisible = true;
+
+			BarcodeScanView.IsScanning = true;
+		}
+
+		public void Handle_OnScanResult(Result result)
+		{
+			if (string.IsNullOrWhiteSpace(result.Text))
+				return;
+
+			if (!result.Text.ToUpperInvariant().StartsWith("WIFI:", StringComparison.Ordinal))
+				return;
+
+			var ssid = GetValueForIdentifier('S', result.Text);
+			var security = GetValueForIdentifier('T', result.Text);
+			var password = GetValueForIdentifier('P', result.Text);
+			var ssidHidden = GetValueForIdentifier('H', result.Text);
+
+			Device.BeginInvokeOnMainThread(() =>
+			{
+				Ssid.Text = ssid;
+
+				switch (security)
+				{
+					case "WPA":
+						Security.SelectedIndex = 0;
+						break;
+					case "WEP":
+						Security.SelectedIndex = 1;
+						break;
+					default:
+						Security.SelectedIndex = 2;
+						break;
+				}
+
+				Password.Text = password;
+				HiddenSsid.IsToggled = !string.IsNullOrWhiteSpace(ssidHidden) && ssidHidden.ToUpperInvariant() == "TRUE";
+			});
+		}
+
+		private string GetValueForIdentifier(char identifier, string haystack)
+		{
+			var startIdx = haystack.IndexOf($"{identifier}:", StringComparison.Ordinal);
+
+			if (startIdx == -1)
+				return "";
+
+			startIdx += 2;
+
+			var length = haystack.IndexOf(';', startIdx) - startIdx;
+
+			return haystack.Substring(startIdx, length);
+		}
+
+		public void ShowHidePassword(object sender, EventArgs e)
+		{
+			Password.IsPassword = !Password.IsPassword;
+		}
+	}
+}
